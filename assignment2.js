50fbedb55f025e05e2af0fbd028fb85fecd51308
Vansh Viraj Bisht <vanshbisht74@gmail.com>
2026-03-19 00:06:49 +0530

    Refactor weather app logging and history functions

--- assignment2.js
+++ assignment2.js
@@ -1,9 +1,8 @@
-let logBox = document.getElementById("log")
-let historyBox = document.getElementById("history")
+let logBox = document.getElementById("logBox")
+let historyBox = document.getElementById("historyBox")
 
 function log(text){
 logBox.innerHTML += text + "<br>"
-console.log(text)
 }
 
 function saveHistory(city){
@@ -14,22 +13,22 @@ if(!list.includes(city)){
 list.push(city)
 }
 
-localStorage.setItem("cities",JSON.stringify(list))
+localStorage.setItem("cities", JSON.stringify(list))
 
 showHistory()
 }
 
 function showHistory(){
 
-historyBox.innerHTML=""
+historyBox.innerHTML = ""
 
 let list = JSON.parse(localStorage.getItem("cities")) || []
 
 list.forEach(function(city){
 
 let btn = document.createElement("button")
-
 btn.innerText = city
+btn.className = "button"
 
 btn.onclick = function(){
 getWeather(city)
@@ -40,44 +39,50 @@ historyBox.appendChild(btn)
 })
 }
 
-async function searchWeather(){
+function searchWeather(){
 
-let city = document.getElementById("city").value
+let city = document.getElementById("cityInput").value
+
+if(city == ""){
+log("enter city name")
+return
+}
 
 getWeather(city)
-
 }
 
 async function getWeather(city){
 
-log("Sync Start")
+log("start")
 
 try{
 
-log("API Start fetching")
+log("fetching data")
 
-let res = await fetch("https://wttr.in/"+city+"?format=j1")
+let res = await fetch("https://wttr.in/" + city + "?format=j1")
 
 let data = await res.json()
 
-log("Process (Microtask)")
+log("data received")
 
-document.getElementById("cityName").innerText = city
-document.getElementById("temp").innerText = data.current_condition[0].temp_C+" °C"
-document.getElementById("weather").innerText = data.current_condition[0].weatherDesc[0].value
-document.getElementById("humidity").innerText = data.current_condition[0].humidity+"%"
-document.getElementById("wind").innerText = data.current_condition[0].windspeedKmph+" km/h"
+document.getElementById("cityText").innerText = city
+document.getElementById("tempText").innerText = data.current_condition[0].temp_C + " C"
+document.getElementById("weatherText").innerText = data.current_condition[0].weatherDesc[0].value
+document.getElementById("humidityText").innerText = data.current_condition[0].humidity
+document.getElementById("windText").innerText = data.current_condition[0].windspeedKmph
 
 saveHistory(city)
 
 }catch(e){
 
-document.getElementById("cityName").innerText="City not found"
+log("error")
+
+document.getElementById("cityText").innerText = "not found"
 
 }
 
 setTimeout(function(){
-log("setTimeout (Macrotask)")
+log("timeout done")
 },0)
 
 }
